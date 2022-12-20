0a89eb43a0b9331a793d5892064ea24b1f51daca
calvinaca <64489982+calvinaca@users.noreply.github.com>
2022-12-20 19:36:26 +0530

    first commit

---
+++ project_VP22CSCI0100070/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>case study </title>
+ <style type="text/css">
+  body {
+   align-content: center;
+  }
+ </style>
+</head>
+<body>
+ <h3><a href="Resume.html" target="_blank">Resume</a></h3>
+ <h3><a href="academicCalender.html" target="_blank">Calender</a></h3>
+ <h3><a href="timeTable.html" target="_blank">Time Table</a></h3>
+ <hr>
+ <h3><a href="#">JavaScript</a></h3>
+ <hr>
+ <h3><a href="#">PHP</a></h3>
+</body>
+</html>
